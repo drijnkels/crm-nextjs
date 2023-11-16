cb6a581a21e8ccf4c05968497f7993bfc6b890d9
drijnkels <daanrijnkels@gmail.com>
2023-11-15 17:25:58 -0700

    Update readme, add favicon and page titles

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import Menu from "./Menu";
 import Header from "./Header";
 import Page from "./Page"
@@ -10,6 +11,9 @@ type LayoutProps = {
 export default function Layout({ page_name, children }: LayoutProps) {
   return (
   <main className="flex min-h-screen">
+    <Head>
+      <title>{page_name}</title>
+    </Head>
     <Menu />
     <div className="flex-1">
       <Header text={page_name} />
