98b2b6527aa8920c5d9c3c30b997bde8fed55e64
drijnkels <daanrijnkels@gmail.com>
2023-11-16 15:36:01 -0700

    Update Text component with more variants

--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -1,7 +1,14 @@
-export default function Text ({ children, variant = 'normal' }: {children: React.ReactNode, variant?: 'normal' | 'heading1'}) {
-  const variants: { [key in 'normal' | 'heading1'] : string} = {
+type TextProps = {
+  children: React.ReactNode,
+  variant?: 'normal' | 'heading1' | 'heading2' | 'heading3'
+}
+
+export default function Text ({ children, variant = 'normal' }: TextProps) {
+  const variants: { [key in 'normal' | 'heading1' | 'heading2' | 'heading3'] : string} = {
     normal: 'whitespace-pre-line',
-    heading1: 'font-semibol text-3xl whitespace-pre-line'
+    heading1: 'font-semibold text-3xl whitespace-pre-line',
+    heading2: 'font-semibold text-2xl whitespace-pre-line',
+    heading3: 'font-medium text-lg whitespace-pre-line'
   };
   return (
     <div className={variants[variant]}>{children}</div>
