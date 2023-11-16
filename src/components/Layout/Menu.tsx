2f8d289151f60e183157abb87c17d49ebf316415
drijnkels <daanrijnkels@gmail.com>
2023-11-16 13:26:34 -0700

    Fix menu background on page with longer content

--- src/components/Layout/Menu.tsx
+++ src/components/Layout/Menu.tsx
@@ -9,13 +9,15 @@ export default function Menu(){
     {url: '/collaborations', text: 'Collaborations', icon: DocumentCheckIcon},
     {url: '/organisations', text: 'Organisations', icon: BuildingOfficeIcon},
     {url: '/users', text: 'Users', icon: UserIcon},
-    {url: '/events', text: 'Events', icon: TicketIcon},
+    // {url: '/events', text: 'Events', icon: TicketIcon},
   ]
   return(
     <div className="bg-zinc-50 h-screen w-[240px] flex flex-col border-r border-zinc-100 text-zinc-500 font-semibold text-[14px]">
-      <div className="px-2 h-[56px] flex items-center justify-center rounded-md border-b border-zinc-200">Tribe Beheer</div>
-      <div className="p-2 flex flex-col">
-        { menuItems.map((item, index) => <MenuItem key={index} Icon={item.icon} url={item.url} text={item.text} />) }
+      <div className="fixed w-[240px] h-full bg-zinc-50">
+        <div className="px-2 h-[56px] flex items-center justify-center rounded-md border-b border-zinc-200">CRM</div>
+        <div className="p-2 flex flex-col">
+          { menuItems.map((item, index) => <MenuItem key={index} Icon={item.icon} url={item.url} text={item.text} />) }
+        </div>
       </div>
     </div>
   )
