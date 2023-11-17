03e8cc148de623dc9f4febb256bcf6a4c7413451
drijnkels <daanrijnkels@gmail.com>
2023-11-17 12:28:09 -0700

    Add Card for Login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
-import Login from "@/components/Forms/Login"
+import Login from "@/components/Forms/Login";
+import Card from "@/components/Elements/Card";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="flex flex-col gap-4">
         <div className="font-bold text-lg">Login</div>
-
-        <Login />
+        <Card>
+          <Login />
+        </Card>
       </div>
     </main>
   )
