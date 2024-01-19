c2e062862a4124e9f0a90932845b5c88d0794171
drijnkels <daanrijnkels@gmail.com>
2024-01-19 16:23:30 -0700

    Implement Login form tests

---
+++ __tests__/components/Login.test.tsx
@@ -0,0 +1,69 @@
+import { vi, describe, it, expect, beforeEach } from "vitest";
+import { render, screen, waitFor } from "@testing-library/react";
+import userEvent from '@testing-library/user-event';
+import Home from "@/app/page"
+import {AppRouterContextProviderMock } from '../app-router-context-provider-mock';
+
+describe('Login', () => {
+
+  beforeEach(() => {
+    render(
+      <Home />
+    );
+  });
+
+  it('should have input input, password input and login button', () => {
+    const emailInput = screen.getByLabelText(/Your email/i)
+    const passwordInput = screen.getByLabelText(/Your password/i)
+    const loginButton = screen.getByRole('button', {name: /Login/i})
+
+    expect(emailInput).toBeDefined()
+    expect(passwordInput).toBeDefined()
+    expect(loginButton).toBeDefined()
+  })
+
+  it('should warn if no email was entered', async () => {
+
+    const emailInput = screen.getByLabelText(/Your email/i)
+    const passwordInput = screen.getByLabelText(/Your password/i)
+    const loginButton = screen.getByRole('button', {name: /Login/i})
+
+    await userEvent.click(loginButton);
+
+    expect(screen.getByText('Email is required')).toBeDefined()
+    expect(screen.getByText('Password is required')).toBeDefined()
+  })
+
+  it('should warn if an invalid email was entered', async () => {
+    const emailInput = screen.getByLabelText(/Your email/i)
+    const passwordInput = screen.getByLabelText(/Your password/i)
+    const loginButton = screen.getByRole('button', {name: /Login/i})
+
+    await userEvent.type(emailInput, 'invalidemail')
+
+    await userEvent.click(loginButton)
+
+    expect(screen.getByText('Please enter a valid email')).toBeDefined()
+  })
+
+  it('should login', async () => {
+    const emailInput = screen.getByLabelText(/Your email/i)
+    const passwordInput = screen.getByLabelText(/Your password/i)
+    const loginButton = screen.getByRole('button', {name: /Login/i})
+
+    await userEvent.type(emailInput, 'valid@email.com');
+    await userEvent.type(passwordInput, 'validpassword')
+
+    await userEvent.click(loginButton)
+
+    const errorMessages = [
+      screen.queryByText('Email is required'),
+      screen.queryByText('Password is required'),
+      screen.queryByText('Please enter a valid email'),
+      ]
+    expect(errorMessages[0]).toBeNull()
+    expect(errorMessages[1]).toBeNull()
+    expect(errorMessages[2]).toBeNull()
+  })
+
+});
