56393c749b1806bf4d207135abf7c8d2f4ed63e3
drijnkels <daanrijnkels@gmail.com>
2024-01-19 12:23:11 -0700

    Implement organisation table tests

---
+++ __tests__/components/Tables/OrgasTable.test.tsx
@@ -0,0 +1,41 @@
+import { describe, it, expect, beforeEach, afterEach } from 'vitest'
+import { render, screen, cleanup } from '@testing-library/react'
+
+import { mockOrganisations } from "../../mocks/OrganisationsDataMock";
+import OrgasTable from "@/components/Tables/OrgasTable";
+import {OrgaHeaderName} from "@/types/OrganisationTypes";
+
+const headers: Array<OrgaHeaderName> = ['name', 'city', 'num_users', 'num_topics', 'url'];
+
+describe('OrgasTable', () => {
+
+  beforeEach(() => {
+    render (
+      <OrgasTable title="Organisations" headers={headers} rows={mockOrganisations} loading_message="Loading organisations" />
+    );
+  });
+
+  afterEach(cleanup);
+
+  it('Does table render correctly', () => {
+    expect(screen.getByText('Name')).toBeDefined();
+    expect(screen.getByText('City')).toBeDefined();
+    expect(screen.getByText('Users')).toBeDefined();
+    expect(screen.getByText('Topics')).toBeDefined();
+    expect(screen.getAllByText('View')).toBeDefined();
+    expect(screen.getAllByText('View').length).toEqual(6);
+  })
+
+  it('Data is shown', () => {
+    expect(screen.getByText('EcoLogiX Solutions')).toBeDefined();
+    expect(screen.getByText(1)).toBeDefined();
+    expect(screen.getByText(5)).toBeDefined();
+    expect(screen.getByText(6)).toBeDefined();
+    expect(screen.getByText(10)).toBeDefined();
+  })
+
+  it('Urls are formatted correctly', () => {
+    const links = screen.getAllByRole('link', { name: /View/i});
+    expect(links[0].getAttribute('href')).toBe('/organisations/knrbpkdwne')
+  })
+})
---
+++ __tests__/mocks/OrganisationsDataMock.ts
@@ -0,0 +1,9 @@
+import {OrganisationForTable} from "@/types/OrganisationTypes";
+
+export const mockOrganisations: OrganisationForTable[] = [
+  {eid: 'knrbpkdwne', name: 'EcoLogiX Solutions', city: 'New York', num_users: 1, num_topics: 6, active: 'Active', created_on: '2023-11-08 16:35:12', url: '/organisations/knrbpkdwne'},
+  {eid: 'naxrqaawqy', name: 'DigitalVista Labs', city: 'Denver', num_users: 2, num_topics: 7, active: 'Active', created_on: '2023-11-08 16:30:29', url: '/organisations/naxrqaawqy'},
+  {eid: 'kegrpbiwwf', name: 'WellnessWorks Pro', city: 'San Fransisco', num_users: 3, num_topics: 8, active: 'Active', created_on: '2023-11-08 16:28:55', url: '/organisations/kegrpbiwwf'},
+  {eid: 'lgfjvnvxdh', name: 'MarketMasters International', city: 'Seattle', num_users: 4, num_topics: 9, active: 'Active', created_on: '2023-11-08 16:24:02', url: '/organisations/lgfjvnvxdh'},
+  {eid: 'aeljedumhf', name: 'IntelliServe Innovations', city: 'Los Angeles', num_users: 5, num_topics: 10, active: 'Active', created_on: '2023-11-08 16:20:38', url: '/organisations/aeljedumhf'},
+];
